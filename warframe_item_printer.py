974637ed6a58d8c116edad28379d275bcce6089b
Jeremy Wright <uniquelyjeremy@gmail.com>
2018-10-06 16:13:43 -0500

    Added 0 seller/buyer detection, forma detection, message and status

--- warframe_item_printer.py
+++ warframe_item_printer.py
@@ -1,5 +1,6 @@
 from tabulate import tabulate
 from warframe_item import WarframeItem
+from collections import OrderedDict
 
 class WarframeItemPrinter:
     items = []
@@ -9,12 +10,28 @@ class WarframeItemPrinter:
 
     def tabulate(self, buyer_stats=True, seller_stats=True, ducats=True):
         data = []
+        
         for item in self.items:
             values = item.get_data()
-            data += [[values['name'], values['seller_stats']['min'], values['seller_stats']['mean'] + u" \u00B1 " + values['seller_stats']['stdev'], values['buyer_stats']['max'], values['buyer_stats']['mean'] + u" \u00B1 " + values['buyer_stats']['stdev'], values['ducats']]]
+            if values['status'] == 200:
+                data += [[
+                    values['name'],
+                    values['seller_stats']['min'],
+                    values['seller_stats']['mean'] + u" \u00B1 " + values['seller_stats']['stdev'],
+                    values['buyer_stats']['max'],
+                    values['buyer_stats']['mean'] + u" \u00B1 " + values['buyer_stats']['stdev'],
+                    values['ducats'],
+                    values['message']
+                ]]
+            else:
+                data += [[
+                    values['name'],
+                    0, 0, 0, 0, 0,
+                    values['message']
+                ]]
 
-        print(tabulate(data, headers=["Name", "Min Sell Price", "Sell Price Average", "Buy Price Max", "Buy Price Average", "Ducats"]))
+        print(tabulate(data, headers=["Name", "Min Sell Price", "Sell Price Average", "Max Buy Price", "Buy Price Average", "Ducats", "Message"]))
         
-printer = WarframeItemPrinter([WarframeItem('Neo B1 Intact'), WarframeItem('Saryn Prime Chassis Blueprint'), WarframeItem('Tigris Prime Blueprint')])
+printer = WarframeItemPrinter([WarframeItem('Paris Prime Lower Limb'), WarframeItem('Saryn Prime Chassis Blueprint'), WarframeItem('Tigris Prime Blueprint'), WarframeItem('Forma Blueprint')])
 
 printer.tabulate()
