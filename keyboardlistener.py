70a3f1da4f93d61ec0537bd8db9235648a4dda4e
Nathan <healbadbad@yahoo.com>
2018-10-06 20:21:51 -0500

    added code to draw a selector box on screen

--- keyboardlistener.py
+++ keyboardlistener.py
@@ -1,37 +1,79 @@
 from pynput import keyboard
+from pynput.mouse import Button, Controller
 import primepartlookup
+from selectorbox import WarRectSelector
+import multiprocessing
+import os
+
+#p1 = 
+class WarBot:
+    def __init__(self,):
+        self.mouse = Controller()
+        self.relicOCR = primepartlookup.RelicOCR()
+        self.ocrSelectorState = False
+        self.selectorOverlay = None
+        self.selectorOverlayThread = None
+
+        self.killSelector = multiprocessing.Value('b', False)
 
 
-relicOCR = primepartlookup.RelicOCR()
 
-def on_press(key):
-    try:
-        #print('alphanumeric key {0} pressed'.format(
-        #    key.char))
-        if key.char == '\\':
-            print("Exiting...")
-            return False
-        if key.char == '[':
-            relicOCR.lookup_primeparts()
-        if key.char == ']':
-            relicOCR.lookup_primeparts(debug=True)
+    def on_press(self, key):
+        try:
+            #print('alphanumeric key {0} pressed'.format(
+            #    key.char))
+            if key.char == '\\':
+                print("Exiting...")
+                return False
+            if key.char == '[':
+                self.relicOCR.lookup_primeparts()
+            if key.char == ']':
+                self.relicOCR.lookup_primeparts(debug=True)
+            if key.char == 'p':
+                print("ocrSelectorState: " + str(self.ocrSelectorState))
+                print('module name:', __name__)
+                print('parent process:', os.getppid())
+                print('process id:', os.getpid())
+                if self.ocrSelectorState:
+                    print("Supposed to kill selector")
+                    self.killSelector.value = True
+                    self.selectorOverlayThread.join()
+                    print("after join")
+
+                    #self.selectorOverlay = None
+                    pass
+                else:
+                    print("starting OCR Selector")
+                    self.killSelector.value = False
+
+                    self.selectorOverlay = WarRectSelector(self.killSelector)
+                    self.selectorOverlayThread = multiprocessing.Process(target=self.selectorOverlay.create_Selector, args=()) 
+                    self.selectorOverlayThread.start()
+                    #self.selectorOverlayThread.join()
+                    print("here")
+
+                self.ocrSelectorState = not self.ocrSelectorState
 
 
-    except AttributeError:
-        #print('special key {0} pressed'.format(
+
+        except AttributeError:
+            #print('special key {0} pressed'.format(
+            #    key))
+            pass
+
+    def on_release(self, key):
+        #print('{0} released'.format(
         #    key))
+        #if key == keyboard.Key.esc:
+            # Stop listener
+        #    return False
         pass
 
-def on_release(key):
-    #print('{0} released'.format(
-    #    key))
-    #if key == keyboard.Key.esc:
-        # Stop listener
-    #    return False
-    pass
-
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+if __name__ == '__main__':
+    warbot = WarBot()
+
+    with keyboard.Listener(
+            on_press=warbot.on_press,
+            on_release=warbot.on_release) as listener:
+        listener.join()
---
+++ selectorbox.py
@@ -0,0 +1,95 @@
+import tkinter
+import win32api, win32con, win32gui, pywintypes
+import time
+from pynput.mouse import Button, Controller
+import multiprocessing
+
+
+class WarRectSelector:
+
+	def __init__(self, toDie):
+		print("Instantiating WarRectSelector")
+		self.toDie = toDie
+		pass
+
+	def create_Selector(self):
+		print("create WarRectSelector")
+		self.root = tkinter.Tk()
+		self.mouse = Controller()
+		self.rectcoords = [self.mouse.position[0], self.mouse.position[1], 100,100]
+
+		self.canvas = tkinter.Canvas(self.root, width=2560, height=1440, highlightthickness=0)
+		self.canvas.configure(background='white')
+		self.root.configure(background='white')
+		self.a = self.canvas.create_rectangle(0, 0, 100, 100, outline='red')# fill='red')
+		#canvas.master.attributes("-alpha", .30)
+		self.canvas.pack()
+		self.canvas.master.overrideredirect(True)
+		#canvas.master.geometry("+250+250")
+		self.canvas.master.lift()
+		self.canvas.master.wm_attributes("-topmost", True)
+		self.canvas.master.wm_attributes("-disabled", True)
+		self.canvas.master.wm_attributes("-transparentcolor", "white")
+
+
+		hWindow = pywintypes.HANDLE(int(self.root.frame(), 16))
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+		# The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+		#exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+		exStyle =  win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+		win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+
+		#label.pack()
+		#label.mainloop()
+		self.canvas.pack()
+		#root.mainloop()
+		self.draw()
+		while True:
+			self.root.update_idletasks()
+			self.root.update()
+			if self.toDie.value:
+				print("KILLING SELECTOR")
+				self.root.destroy()
+				exit()
+				print("after exit?")
+		    #time.sleep(1)
+
+	def draw(self):
+		#self.canvas.pack()
+		#self.canvas.update()
+		self.rectcoords[2] = self.mouse.position[0]
+		self.rectcoords[3] = self.mouse.position[1]
+		self.canvas.coords(self.a, self.rectcoords)
+		self.canvas.after(1, self.draw)
+
+class BasicCanvas:
+	def __init__(self):
+		self.root = tkinter.Tk()
+		self.canvas = tkinter.Canvas(self.root, width=500, height=500)
+		self.a = self.canvas.create_rectangle(0, 0, 100, 100, fill='red')
+
+		self.canvas.master.wm_attributes("-transparentcolor", "white")
+		self.canvas.master.wm_attributes("-topmost", True)
+
+		#canvas.master.overrideredirect(True)
+
+		self.canvas.pack()
+
+		hWindow = pywintypes.HANDLE(int(self.canvas.master.frame(), 16))
+		# http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+		# The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+		exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+		win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+
+	def draw(self):
+		self.canvas.pack()
+		self.canvas.update()
+		self.canvas.after(1, self.draw)
+		#root.mainloop()
+
+if __name__ == "__main__":
+	selector = WarRectSelector(False)
+	selector.create_Selector()
+	#selector=BasicCanvas()
+	#selector.draw()
+	#selector.root.mainloop()
